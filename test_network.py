269c32b9ecf8c53a05205c29420c0f58718d891c
David Weber <dsw7@sfu.ca>
2022-07-21 13:27:42 -0700

    Add assert to test

--- test_network.py
+++ test_network.py
@@ -1,7 +1,9 @@
-from os import getcwd, EX_OK
+from os import getcwd, EX_OK, path
 from typing import TypeVar
 from logging import getLogger
-from subprocess import Popen, DEVNULL
+from tempfile import gettempdir
+from json import load
+from subprocess import Popen, PIPE
 import docker
 import pytest
 
@@ -52,7 +54,18 @@ class TestClient:
 
         LOGGER.info('Running command: "%s"', ' '.join(command))
 
-        process = Popen(command, stdout=DEVNULL)
-        process.communicate()
+        process = Popen(command, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+
+        if stdout:
+            LOGGER.info('Command returned stdout: "%s"', stdout.decode())
+
+        if stderr:
+            LOGGER.warning('Command returned stderr: "%s"', stderr.decode())
 
         assert process.returncode == EX_OK
+
+        with open(path.join(gettempdir(), 'ping_results.json')) as f:
+            results = load(f)
+
+        print(results)
